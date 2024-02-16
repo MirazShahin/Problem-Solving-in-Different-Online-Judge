f6c2ebd114237f5222f67e8e52a7e60c911d5e88
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-02-16 14:41:24 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,11 +1,9 @@
 #include <bits/stdc++.h>
-using namespace std;
 #define FastIO                  \
   ios_base::sync_with_stdio(0); \
   cin.tie(0);                   \
   cout.tie(0);
 
-#define ll        long long
 #define vll        vector<ll>
 #define nn         '\n'
 
@@ -17,12 +15,19 @@ using namespace std;
 #define no         cout << "No\n"
 #define sp         " "
 
+#define sz(x)      (int)x.size()
 #define pi         3.1415926535897932384626433832795
 #define all(x)     (x).begin(), (x).end()
 #define rall(x)    (x).rbegin(), (x).rend()
 
 
+using namespace std;
+using ll = long long int;
+using ld = long double;
+using ull = unsigned long long;
+
 ///--------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>----Modular Arithmetic----<<<<<<<<<<<<<<<<<<<<<<<<<---------------
+
 const  ll MOD = (ll) 1e9 + 7;
 inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
 inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
