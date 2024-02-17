1c686f1b068e0325fa19e54f6164f219d61927a0
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-02-18 00:27:12 +0600

    Create Template.cpp

---
+++ My Template/Template.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#define FastIO                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+
+#define vll        vector<ll>
+#define nn         '\n'
+
+#define gcd(a,b)   __gcd(a,b)
+#define lcm(a,b)   (a*b) / gcd(a,b)
+#define YES        cout << "YES\n"
+#define NO         cout << "NO\n"
+#define yes        cout << "Yes\n"
+#define no         cout << "No\n"
+#define sp         " "
+
+#define sz(x)      (int)x.size()
+#define pi         3.1415926535897932384626433832795
+#define all(x)     (x).begin(), (x).end()
+#define rall(x)    (x).rbegin(), (x).rend()
+
+
+using namespace std;
+using ll = long long int;
+using ld = long double;
+using ull = unsigned long long;
+
+///--------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>----Modular Arithmetic----<<<<<<<<<<<<<<<<<<<<<<<<<---------------
+
+const  ll MOD = (ll) 1e9 + 7;
+inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
+inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
+inline ll modAdd(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a+b)%MOD; }
+inline ll modSub(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while(p) { if(p&1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, MOD-2); }  /// When MOD is prime.
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+///--------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>----The End----<<<<<<<<<<<<<<<<<<<<<<<<<---------------
+
+const ll N = (ll) 1e6 + 5;
+bitset<N >marked;
+vll  prime;
+
+///--------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>----MiRaZ_HoSsAiN_ShAhIn----<<<<<<<<<<<<<<<<<<<<<-----------------
+
+void Sieve()
+{
+    for(int i = 4; i <= N; i+=2) {
+        marked[i] = 1;
+    }
+    for(int i = 3; i * i <= N; i+= 2) {
+        if(!marked[i])
+        for(int j = i * i; j <= N; j += (i << 1)) {
+            marked[j] = 1;
+        }
+    }
+    prime.push_back(2);
+    for(int i = 3; i <= N; i+=2) {
+        if(!marked[i]) {
+            prime.push_back(i);
+        }
+    }
+}
+bool isPrime(ll n) {
+    if(n < 2) return false;
+    return !marked[n];
+}
+///---------------------------------------->>>>>---Start---<<<<<---------------------------------------------
+void solve()
+{
+
+}
+int32_t main()
+{
+    FastIO;
+    Sieve();
+    int test_case;
+    cin >> test_case;
+    int Case = 1;
+    while(test_case--) {
+//    cout << "Case " << Case << ": ";
+        solve();
+        Case++;
+    }
+
+    return 0;
+}
