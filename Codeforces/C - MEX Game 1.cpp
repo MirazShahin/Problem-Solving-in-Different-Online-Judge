53433d29549e5bd899b8cf3a769a747777c90efe
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-04-08 19:04:04 +0600

    Create C - MEX Game 1.cpp

---
+++ Codeforces/C - MEX Game 1.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+#define FastIO                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+
+#define vii        vector<int>
+#define vll        vector<ll>
+#define nn         '\n'
+
+#define gcd(a,b)   __gcd(a,b)
+#define lcm(a,b)   (a*b) / gcd(a,b)
+#define sp         " "
+
+#define sz(x)      (int)x.size()
+#define all(x)     (x).begin(), (x).end()
+#define rall(x)    (x).rbegin(), (x).rend()
+
+
+using namespace std;
+using ll = long long int;
+///------------------------------>>>>>---Start---<<<<<---------------------------------------------
+void solve()
+{
+    int n;
+    cin >> n;
+    vii v(n);
+    for(int i = 0; i < n; i++ ) cin >> v[i];
+
+    int zero_count = 0;
+    for(int i = 0; i < n; i++ ) {
+        if(v[i] == 0) zero_count++;
+    }
+    if(zero_count == 0) {
+        cout << 0 << nn;
+        return;
+    }
+    sort(all(v));
+    map<int, int> mp;
+    for(auto u : v) mp[u]++;
+
+    vii single;
+    for(auto [u , it] : mp) {
+        if(it == 1) {
+            single.push_back(u);
+        }
+    }
+    vii mingle;
+    for(auto [u , it] : mp) {
+        if(it > 1) {
+            mingle.push_back(u);
+        }
+    }
+    if(sz(mp) == n) {
+        cout << 1 << nn;
+        return;
+    }
+    sort(all(single));
+    vii alice;
+    for(int i = 0; i < sz(single); i += 2) {
+        alice.push_back(single[i]);
+    }
+    for(auto u : mingle ) {
+        alice.push_back(u);
+    }
+    sort(all(alice));
+    int mex_ans = 0;
+    for(int i = 0; i < sz(alice); i++ ) {
+        if(alice[i] == i) {
+            mex_ans = mex_ans + 1;
+        }
+    }
+    cout << mex_ans << nn;
+}
+int32_t main()
+{
+    FastIO;
+
+    int test_case = 1;
+    cin >> test_case;
+    int Case = 1;
+    while(test_case--) {
+//    cout << "Case " << Case << ": ";
+        solve();
+        Case++;
+    }
+
+    return 0;
+}
