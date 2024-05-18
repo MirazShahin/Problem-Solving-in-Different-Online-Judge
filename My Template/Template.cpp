daade6394120420e52d8307c3b6313ad1f8f4313
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-05-18 16:05:47 +0600

    Update Template.cpp

--- My Template/Template.cpp
+++ My Template/Template.cpp
@@ -3,23 +3,16 @@
   ios_base::sync_with_stdio(0); \
   cin.tie(0);                   \
   cout.tie(0);
-
+#define vii        vector<int>
 #define vll        vector<ll>
 #define nn         '\n'
 
 #define gcd(a,b)   __gcd(a,b)
 #define lcm(a,b)   (a*b) / gcd(a,b)
-#define YES        cout << "YES\n"
-#define NO         cout << "NO\n"
-#define yes        cout << "Yes\n"
-#define no         cout << "No\n"
 #define sp         " "
 
 #define sz(x)      (int)x.size()
-#define pi         3.1415926535897932384626433832795
 #define all(x)     (x).begin(), (x).end()
-#define rall(x)    (x).rbegin(), (x).rend()
-
 
 using namespace std;
 using ll = long long int;
@@ -27,7 +20,6 @@ using ld = long double;
 using ull = unsigned long long;
 
 ///--------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>----Modular Arithmetic----<<<<<<<<<<<<<<<<<<<<<<<<<---------------
-
 const  ll MOD = (ll) 1e9 + 7;
 inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
 inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
@@ -38,13 +30,27 @@ inline ll modInverse(ll a) { return modPow(a, MOD-2); }  /// When MOD is prime.
 inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
 
 ///--------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>----The End----<<<<<<<<<<<<<<<<<<<<<<<<<---------------
-
-const ll N = (ll) 1e6 + 5;
-bitset<N >marked;
-vll  prime;
+const ll N = (ll) 1e6 + 7;
+bitset<N> marked;
+vii  prime;
+int Divisor[N];
+int Divisor_sum[N];
 
 ///--------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>----MiRaZ_HoSsAiN_ShAhIn----<<<<<<<<<<<<<<<<<<<<<-----------------
-
+void divisor_count() {
+    for (int i = 1; i < N; i++) {
+        for(int j = i; j < N; j += i) {
+            Divisor[j]++;
+        }
+    }
+}
+void divisor_sum() {
+    for (int i = 1; i < N; i++) {
+        for(int j = i; j < N; j += i) {
+            Divisor_sum[j] += i;
+        }
+    }
+}
 void Sieve()
 {
     for(int i = 4; i <= N; i+=2) {
@@ -67,17 +73,21 @@ bool isPrime(ll n) {
     if(n < 2) return false;
     return !marked[n];
 }
-///---------------------------------------->>>>>---Start---<<<<<---------------------------------------------
-void solve()
-{
 
+///---------------------------------------->>>>>---Start---<<<<<---------------------------------------------
+void solve() {
+    // Code here....
+
+  
 }
 int32_t main()
 {
     FastIO;
     Sieve();
-    int test_case;
-    cin >> test_case;
+    divisor_count();
+    divisor_sum();
+    int test_case = 1;
+//    cin >> test_case;
     int Case = 1;
     while(test_case--) {
 //    cout << "Case " << Case << ": ";
