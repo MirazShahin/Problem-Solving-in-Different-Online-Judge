22984c2568ca3a8751d9385fe5a2f688267c61fe
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-05-28 22:13:24 +0600

    Create 	D - Binary Cut.cpp

---
+++ Codeforces/D - Binary Cut.cpp
@@ -0,0 +1,61 @@
+
+#include <bits/stdc++.h>
+#define FastIO                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+
+#define vll        vector<ll>
+#define vii        vector<int>
+#define nn         '\n'
+
+#define gcd(a,b)   __gcd(a,b)
+#define lcm(a,b)   (a*b) / gcd(a,b)
+#define sp         " "
+
+#define sz(x)      (int)x.size()
+#define all(x)     (x).begin(), (x).end()
+#define rall(x)    (x).rbegin(), (x).rend()
+
+
+using namespace std;
+using ll = long long int;
+
+void solve() {
+    string s;
+    cin >> s;
+    if(is_sorted(all(s))) {
+        cout << 1 << nn;
+        return;
+    }
+    int ans = 1;
+    for(int i = 0; i < sz(s) - 1; i++ ) {
+        if(s[i] == s[i + 1]) continue;
+        else ans++;
+    }
+    bool check = 0;
+    for(int i = 0; i < sz(s) - 1; i++ ) {
+        if(s.substr(i, 2) == "01") {
+            check = true;
+        }
+    }
+    if(check) {
+        ans--;
+    }
+    cout << ans << nn;
+}
+int32_t main() {
+    FastIO;
+
+    int test_case = 1;
+    cin >> test_case;
+    int Case = 1;
+    while(test_case--) {
+//    cout << "Case " << Case << ": ";
+        solve();
+        Case++;
+    }
+
+    return 0;
+}
+
