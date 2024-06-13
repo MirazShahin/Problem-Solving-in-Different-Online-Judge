ca1ad6faec989adb6f1d078e091e5d29f30ad66e
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-06-13 13:05:02 +0600

    Create Painting Walls.cpp

---
+++ Codechef/Painting Walls.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+	    int a,b,c;
+	    cin >> a >> b >> c;
+	    int x = c/2;
+	    int ar =  a * b;
+	    int ans = x / ar;
+	    cout << ans << endl;
+	}
+
+}
