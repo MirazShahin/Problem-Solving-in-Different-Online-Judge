fe328575b840acc4f0e089a857c4880dc375b209
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-06-15 18:43:23 +0600

    Create B - Ticket Counter.cpp

---
+++ Atcoder/B - Ticket Counter.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define FastIO                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+#define vii        vector<int>
+#define vll        vector<ll>
+#define nn         '\n'
+
+#define gcd(a,b)   __gcd(a,b)
+#define lcm(a,b)   (a*b) / gcd(a,b)
+#define sp         " "
+
+#define sz(x)      (int)x.size()
+#define all(x)     (x).begin(), (x).end()
+
+using namespace std;
+using ll = long long int;
+using ld = long double;
+using ull = unsigned long long;
+
+///...(__________________________(__MS__MiRAZ_847)____________________________)...///
+
+void solve() {
+    ll n, a;
+    cin >> n >> a;
+    vll v(n);
+    for(int i = 0; i < n; i++ )  cin >> v[i];
+    vll ans;
+    ans.push_back(v[0] + a);
+    for(int i = 1; i < n; i++ ) {
+        if(ans.back() > v[i]) {
+            ans.push_back(ans.back() + a);
+        }
+        else {
+            ans.push_back(v[i] + a);
+        }
+    }
+    for(auto u : ans ) cout << u << nn;
+}
+
+int32_t main() {
+    FastIO; 
+
+    int test_case = 1;
+    // cin >> test_case;
+    int Case = 1;
+    while(test_case--) {
+//    cout << "Case " << Case << ": ";
+        solve();
+        Case++;
+    }
+
+    return 0;
+}
+/*
+ * do something instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
