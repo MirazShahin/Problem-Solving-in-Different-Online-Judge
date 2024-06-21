fc8790d5093ef074cf85787304dbcc22aee83521
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-06-21 22:04:26 +0600

    Create A - The number of positions.cpp

---
+++ Codeforces/A - The number of positions.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define FastIO                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+#define vii        vector<int>
+#define vll        vector<ll>
+#define nn         '\n'
+
+#define gcd(a,b)   __gcd(a,b)
+#define lcm(a,b)   (a*b) / gcd(a,b)
+#define sp         " "
+
+#define sz(x)      (int)x.size()
+#define all(x)     (x).begin(), (x).end()
+
+using namespace std;
+using ll = long long int;
+using ld = long double;
+using ull = unsigned long long;
+
+///...(__________________________(__MS__MiRAZ_847)____________________________)...///
+ 
+void solve() {
+    /*
+      21 - 06 - 2024  
+    */
+    int n, a, b;
+    cin >> n >> a >> b;
+    n -= max(a + 1, n - b);
+    cout << n + 1 << nn;
+}
+int32_t main() { 
+    FastIO; 
+    
+    int test_case = 1;
+   // cin >> test_case;
+    int Case = 1;
+    while (test_case--) {
+        solve();
+        Case++;
+    }
+
+    return 0;
+}
+
+/*
+ * do something instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
