9d07d1fdcbc25ec5ef6b8ab45b7246e714cea38f
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-06-29 15:47:58 +0600

    Create 	A - United We Stand.cpp

---
+++ Codeforces/A - United We Stand.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#define FastIO                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+#define vii        vector<int>
+#define vll        vector<ll>
+#define nn         '\n'
+
+#define gcd(a,b)   __gcd(a,b)
+#define lcm(a,b)   (a*b) / gcd(a,b)
+#define sp         " "
+
+#define sz(x)      (int)x.size()
+#define all(x)     (x).begin(), (x).end()
+
+using namespace std;
+using ll = long long int;
+using ld = long double;
+using ull = unsigned long long;
+//debug_______________________________________________________
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+// #else
+// #define dbg(args...)
+///...(__________________________(__MS__MiRAZ_847)____________________________)...///
+ 
+ 
+void solve() {
+    /*
+      Date: 28 - 06 - 2024
+    */
+    int n;
+    cin >> n;
+    vii v(n);
+    for(int i = 0; i < n; i++ ) cin >> v[i];
+    sort(all(v));
+    if(v[0] == v[n - 1]) {
+        cout << -1 << nn;
+        return;
+    }
+     vii a, b;
+     for(int i = 0; i < n; i++ ) {
+        if(v[i] == v[0]) {
+            a.push_back(v[i]);
+        }
+        else {
+            b.push_back(v[i]);
+        }
+    }
+    cout << sz(a) << sp << sz(b) << nn;
+    for(auto u : a) cout << u << sp;
+    cout << nn;
+    for(auto u : b) cout << u << sp;
+    cout << nn;
+     
+}
+int32_t main() {
+    FastIO;
+
+    int test_case = 1;
+     cin >> test_case;
+    int Case = 1;
+    while(test_case--) {
+//    cout << "Case " << Case << ": ";
+        solve();
+        Case++;
+    }
+
+    return 0;
+}
+/*
+ * do something instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
