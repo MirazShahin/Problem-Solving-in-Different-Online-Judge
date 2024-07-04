9673f492faf11e9359cbdc50f42df895961c054e
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-07-04 10:19:24 +0600

    Create A - Extremely Round.cpp

---
+++ Codeforces/A - Extremely Round.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+#define FastIO                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+#define vii        vector<int>
+#define vll        vector<ll>
+#define nn         '\n'
+
+#define gcd(a,b)   __gcd(a,b)
+#define lcm(a,b)   (a*b) / gcd(a,b)
+#define sp         " "
+
+#define sz(x)      (int)x.size()
+#define all(x)     (x).begin(), (x).end()
+
+using namespace std;
+using ll = long long int;
+using ld = long double;
+using ull = unsigned long long;
+
+//debug_______________________________________________________
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+// #else
+// #define dbg(args...)
+///...(__________________________(__MS__MiRAZ_847)____________________________)...///
+bool digit_checker(int n) {
+    int x = n;
+    int cnt = 0;
+    while(x > 0) {
+        int rem = x % 10;
+        if(rem != 0) cnt++;
+        x /= 10;
+    }
+    return cnt == 1;
+}
+vector<int> v;
+void calc() {
+    for(int i = 1; i < 1000000; i++ ) {
+        if(digit_checker(i)) v.push_back(i);
+    }
+}
+void solve() {
+    /*
+      Date: 29 - 06 - 2024
+    */
+    int n;
+    cin >> n;
+    int ans = 0;
+    //for(int i = 1; i <= n; i++ ) cout << Array[i] << sp;
+    for(auto u : v) {
+        if(u <= n) ans++;
+        else break;
+    } 
+     cout << ans  << nn;
+ }
+int32_t main() {
+    FastIO; 
+    calc();
+    int test_case = 1;
+    cin >> test_case;
+    int Case = 1;
+    while(test_case--) {
+//    cout << "Case " << Case << ": ";
+        solve();
+        Case++;
+    }
+
+    return 0;
+}
+/*
+ * do something instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
