6f5f5ab9b8808a2f84d9ba90d99cf881ce30f22b
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-09-12 21:10:19 +0600

    Create orderset or order multiset.cpp

---
+++ My Template/orderset or order multiset.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+template <typename T> using om_set = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve() { 
+   
+}
+int32_t main() { 
+    int test_case = 1;
+    //cin >> test_case;
+    int Case = 1;
+    while (test_case--) {
+        solve();
+        Case++;
+    }
+
+    return 0;
+}
