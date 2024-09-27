9f0ff57197ed4b0b3264795e88cad72f2681c2f4
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-09-28 01:37:17 +0600

    Create A - Max Plus Size.cpp

---
+++ Codeforces/A - Max Plus Size.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define FastIO                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0); 
+#define nn  '\n'
+using ll = long long; 
+using namespace std; 
+
+//debug_______________________________________________________
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+// #else
+// #define dbg(args...)
+///__________________________________The End__________________________________|||
+
+void solve() { 
+    int n; cin >> n;
+    vector<int> v(n), w;
+    for(int i = 0; i < n; i++ ) cin >> v[i];
+    int boro1 = 0, boro2 = 0;
+    int cnt1 = 0, cnt2 = 0;
+    for(int i = 0; i < n; i += 2) {
+        boro1 = max(boro1, v[i]);
+        cnt1++;
+    }
+    for(int i = 1; i < n; i += 2) {
+        boro2 = max(boro2, v[i]);
+        cnt2++;
+    }
+   
+    cout << max(boro1 + cnt1, boro2 + cnt2) << nn;
+}
+int32_t main() {
+    FastIO;
+ 
+    int test_case = 1;
+    cin >> test_case;
+    int Case = 1;
+    while(test_case--) {
+//    cout << "Case " << Case << ": ";
+        solve();
+        Case++;
+    }
+
+    return 0;
+}
+/*
+ * do something instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
