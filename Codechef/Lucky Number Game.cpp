c2a551931f0998da1766937f9e967e102f54828f
Miraz Hossain Shahin <115337165+MirazShahin@users.noreply.github.com>
2024-10-15 02:47:40 +0600

    Create Lucky Number Game.cpp

---
+++ Codechef/Lucky Number Game.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+#define FastIO                  \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+#define vii        vector<int>
+#define vll        vector<ll>
+#define nn         '\n'
+
+#define gcd(a,b)   __gcd(a,b)
+#define lcm(a,b)   (a*b) / gcd(a,b)
+#define sp         " "
+
+#define sz(x)      (int)x.size()
+#define all(x)     (x).begin(), (x).end()
+
+using namespace std;
+using ll = long long int;
+using ld = long double;
+using ull = unsigned long long;
+
+const  ll MOD = (ll) 1e9 + 7; 
+// Direction Array...
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1}; // 8-direction.......
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[]= {1,-1,0,0}; // 4-direction...........
+int dy4[]= {0,0,1,-1};
+
+//Modular Arithmetic__________________________________________
+inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
+inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
+inline ll modAdd(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a+b)%MOD; }
+inline ll modSub(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while(p) { if(p&1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, MOD-2); }  /// When MOD is prime.
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+
+//debug_______________________________________________________
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+// #else
+// #define dbg(args...)
+///__________________________________The End__________________________________|||
+
+void solve() {
+    int n, a, b; cin >> n >> a >> b;
+    vii v(n);  
+    for(int i = 0; i < n; i++ ) {
+        cin >> v[i]; 
+    }
+    int both = 0;
+    int alice = 0;
+    int bob = 0;
+    for(auto &it : v) {
+        if(it % a == 0 and it % b == 0) {
+            both++;
+        }
+        else if(it % a == 0) {
+            bob++;
+        }
+        else if(it % b == 0) {
+            alice++;
+        } 
+    }
+    if(a == b) {
+        if(both == 0) {
+            cout << "ALICE\n";
+        }
+        else if(both % 2 ) cout << "BOB\n";
+        else cout << "ALICE\n";
+        return;
+    }
+    if(alice == 0 and bob == 0 and both == 0) {
+        cout << "ALICE\n";
+        return;
+    }
+    if(bob == 0 and alice == 0 and both > 0) {
+        if(both % 2) cout << "BOB\n";
+        else cout << "ALICE\n";
+    }
+    else if(both == 0) {
+        if(alice >= bob) cout << "ALICE\n";
+        else cout << "BOB\n";
+    }
+    else { 
+        if(bob >= alice ) cout << "BOB\n";
+        else cout << "ALICE\n";   
+    }
+} 
+int32_t main() {
+    FastIO;
+ 
+    int test_case = 1;
+    cin >> test_case;
+    int Case = 1;
+    while(test_case--) {
+//    cout << "Case " << Case << ": ";
+        solve();
+        Case++;
+    }
+
+    return 0;
+}
+/*
+ * do something instead of nothing and stay organized
+ * WRITE STUFF DOWN
+ * DON'T GET STUCK ON ONE APPROACH
+ */
